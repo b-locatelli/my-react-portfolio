06f2121e379871e7a217c29efb3e620a2bd21e2c
Baily locatelli <bailylocatelli@gmail.com>
2023-08-14 18:02:22 -0700

    working on pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter basename="/"> 
       <Header />
     <Routes>
-      <Route path="/" element={<AboutMe />} />
+      <Route path="/aboutme" element={<AboutMe />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/resume" element={<Resume />} />
